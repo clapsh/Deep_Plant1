0814993a37be7b35d8c38ab6fe0efeff037c4768
SincerityHun <120380703+SincerityHun@users.noreply.github.com>
2023-07-10 15:53:16 +0900

    Update db_connect.py

--- Backend/db_connect.py
+++ Backend/db_connect.py
@@ -21,34 +21,23 @@ class Species(Base):
     value = Column(String(255))
 
 
-class LargeDivision(Base):
-    __tablename__ = "largeDivision"
+class PrimalCuts(Base):
+    __tablename__ = "primalCuts"
     id = Column(Integer, primary_key=True)
+    speciesId = Column(Integer,ForeignKey("species.id"),primary_key=True)
     value = Column(String(255))
 
 
-class SmallDivision(Base):
-    __tablename__ = "smallDivision"
+class SecondaryCuts(Base):
+    __tablename__ = "secondaryCuts"
     id = Column(Integer, primary_key=True)
+    primalId = Column(Integer,ForeignKey("primalCuts.id"),primary_key=True)
     value = Column(String(255))
 
-
-class Probexpt(Base):
-    __tablename__ = "probexpt"
-    id = Column(String(255), ForeignKey("meat.id"), primary_key=True)
-    seqno = Column(Integer)
-
-
-class FreshmeatSenseval(Base):
-    __tablename__ = "freshmeatSenseval"
-    id = Column(String(255), ForeignKey("meat.id"), primary_key=True)
-    seqno = Column(Integer)
-
-
-class HeatedmeatSenseval(Base):
-    __tablename__ = "heatedmeatSenseval"
-    id = Column(String(255), ForeignKey("meat.id"), primary_key=True)
-    seqno = Column(Integer)
+class GradeNum(Base):
+    __tablename__ = "gradeNum"
+    id = Column(Integer, primary_key=True)
+    value = Column(String(255))
 
 
 class Meat(Base):
@@ -56,40 +45,47 @@ class Meat(Base):
     id = Column(String(255), primary_key=True)
     userId = Column(String(255))
     createdAt = Column(DateTime)
-    traceNumber = Column(String(255))
+    traceNum = Column(String(255))
     farmAddr = Column(String(255))
-    butcheryPlaceNm = Column(String(255))
+    butcheryPlaceNum = Column(String(255))
     butcheryYmd = Column(DateTime)
     birthYmd = Column(DateTime)
     sexType = Column(Integer)
+
     species = Column(Integer, ForeignKey("species.id"))
-    largeDivision = Column(Integer, ForeignKey("largeDivision.id"))
-    smallDivision = Column(Integer, ForeignKey("smallDivision.id"))
+    primalCuts = Column(Integer, ForeignKey("primalCuts.id"))
+    secondaryCuts = Column(Integer, ForeignKey("secondaryCuts.id"))
+    gradeNum = Column(Integer,ForeignKey("gradeNum.id"))
+
+    freshmeat_seqno = Column(Integer)
+    heatedmeat_seqno = Column(Integer)
     probexpt_seqno = Column(Integer)
 
 
 class HistoryFreshmeatSenseval(Base):
     __tablename__ = "historyFreshmeatSenseval"
-    id = Column(Integer, ForeignKey("freshmeatSenseval.seqno"), primary_key=True)
+    id = Column(Integer, ForeignKey("meat.freshmeat_seqno"), primary_key=True)
     createdAt = Column(DateTime)
+    userId = Column(String(255),ForeignKey("users.userId"))
     freshMeatId = Column(
-        String(255), ForeignKey("freshmeatSenseval.id"), primary_key=True
+        String(255), ForeignKey("meat.id"), primary_key=True
     )
     period = Column(Integer)
     marbling = Column(Float)
     color = Column(Float)
     texture = Column(Float)
     surfaceMoisture = Column(Float)
-    total = Column(Float)
+    overall = Column(Float)
 
 
 class HistoryHeatedmeatSenseval(Base):
     __tablename__ = "historyHeatedmeatSenseval"
-    id = Column(Integer, ForeignKey("heatedmeatSenseval.seqno"), primary_key=True)
+    id = Column(Integer, ForeignKey("meat.heatedmeat_seqno"), primary_key=True)
     createdAt = Column(DateTime)
+    userId = Column(String(255),ForeignKey("users.userId"))
     period = Column(Integer)
     heatedMeatId = Column(
-        String(255), ForeignKey("heatedmeatSenseval.id"), primary_key=True
+        String(255), ForeignKey("meat.id"), primary_key=True
     )
     flavor = Column(Float)
     juiciness = Column(Float)
@@ -100,11 +96,11 @@ class HistoryHeatedmeatSenseval(Base):
 
 class HistoryProbexptSenseval(Base):
     __tablename__ = "historyProbexptSenseval"
-    id = Column(Integer, ForeignKey("probexpt.seqno"), primary_key=True)
+    id = Column(Integer, ForeignKey("meat.probexpt_seqno"), primary_key=True)
     createdAt = Column(DateTime)
-    userId = Column(String(255))
+    userId = Column(String(255),ForeignKey("users.userId"))
     period = Column(Integer)
-    probexptId = Column(String(255), ForeignKey("probexpt.id"), primary_key=True)
+    probexptId = Column(String(255), ForeignKey("meat.id"), primary_key=True)
     L = Column(Float)
     a = Column(Float)
     b = Column(Float)
@@ -129,7 +125,7 @@ class UserType(Base):
 
 
 class User(Base):
-    __tablename__ = "_user"
+    __tablename__ = "users"
     userId = Column(String(255), primary_key=True)
     createdAt = Column(DateTime)
     updatedAt = Column(DateTime)
